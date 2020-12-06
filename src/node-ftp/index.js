eaf1c7b022500480ac083c6f17f5fcc94341290d
Yogesh <yogeshjog304@gmail.com>
2020-12-06 22:00:31 +0530

    code modifications

--- src/node-ftp/index.js
+++ src/node-ftp/index.js
@@ -7,7 +7,7 @@ require('dotenv/config')
 
 const express = require('express')
 const cors = require('cors')
-const fs = require('fs');
+//const fs = require('fs');
 const app = express()
 app.disable("x-powered-by");
 const port = process.env.port || 3000
@@ -27,19 +27,24 @@ console.log(`starting server at http://localhost:${port}/`)
 app.use(express.static(__dirname + "/"))
 
 app.use('/ftp', serveIndex(dir + '/'));
-app.use('/static', express.static('public'));
+//app.use('/static', express.static('public'));
 
+app.get('/ftp/*', function(req, res){
+  var resource=req.url.substr(4);
+  const file = `${process.env.dir || "public"}${resource}`;
+  res.download(file); // Set disposition and send it.
+});
 // Listen
 app.listen(port)
 
 
-function getFileNames(dir) {
+// function getFileNames(dir) {
 
-  const testFolder = 'C:\\Softwares';
-  fs.readdir(testFolder, (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-    });
-  });
+//   const testFolder = 'C:\\Softwares';
+//   fs.readdir(testFolder, (err, files) => {
+//     files.forEach(file => {
+//       console.log(file);
+//     });
+//   });
 
-}
+// }
