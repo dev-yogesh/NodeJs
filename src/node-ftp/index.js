5334bf724ec9af4207b5792b5e0bd4e676fc6c75
Yogesh <yogeshjog304@gmail.com>
2020-12-06 22:39:24 +0530

    code cleanup

--- src/node-ftp/index.js
+++ src/node-ftp/index.js
@@ -1,32 +1,34 @@
-//#region Config Setup
+//#region config
 //Create a .env file in the  relative path of index.js & access config using process.env.ConfigKey
 
 require('dotenv/config')
 
-//#endregion
-
-const express = require('express')
-const cors = require('cors')
-//const fs = require('fs');
-const app = express()
-app.disable("x-powered-by");
 const port = process.env.port || 3000
 const dir = process.env.dir || "public"
-var serveIndex = require('serve-index');
+//#endregion
 
 
+//#region imports
+const express = require('express')
+const serveIndex = require('serve-index');
+const cors = require('cors')
+//#endregion
+
+//#region app def
+const app = express() 
+app.disable("x-powered-by") //Recovering fingerprints from web application technologies Vulnerability 
 app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+//#endregion
+
 
 app.get('/', (req, res) => {
   res.send(`Server is up and running. <br> <br><a href="${ req.protocol + '://' + req.get('host') + req.originalUrl+"ftp"}">Click here to access ftp</a>`)
 })
-console.log(`starting server at http://localhost:${port}/`)
-
-app.use(express.static(__dirname + "/"))
 
 app.use('/ftp', serveIndex(dir + '/'));
+
 
 app.get('/ftp/*', function (req, res) {
   if (req.url && req.url.length > 4) {
@@ -35,6 +37,8 @@ app.get('/ftp/*', function (req, res) {
   }
 });
 
+
 // Listen
+console.log(`starting server at http://localhost:${port}/`)
 app.listen(port)
 
