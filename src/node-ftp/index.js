1669fee5f65323a9c828ade6c41636172ed24a0e
Yogesh <yogeshjog304@gmail.com>
2020-12-06 15:41:16 +0530

    node ftp using express js and server index

---
+++ src/node-ftp/index.js
@@ -0,0 +1,45 @@
+//#region Config Setup
+//Create a .env file in the  relative path of index.js & access config using process.env.ConfigKey
+
+require('dotenv/config')
+
+//#endregion
+
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs');
+const app = express()
+app.disable("x-powered-by");
+const port = process.env.port || 3000
+const dir=process.env.dir || "public"
+var serveIndex = require('serve-index');
+
+
+app.use(cors())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running...')
+})
+console.log(`starting server at http://localhost:${port}/`)
+
+app.use(express.static(__dirname + "/"))
+
+app.use('/ftp', serveIndex(dir + '/'));
+app.use('/static', express.static('public'));
+
+// Listen
+app.listen(port)
+
+
+function getFileNames(dir) {
+
+  const testFolder = 'C:\\Softwares';
+  fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
+
+}
